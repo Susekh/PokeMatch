b96337bf92edeb084636b1972f5579f006c8146f
Subh <subhranshukhilar@outlook.com>
2025-05-17 22:31:33 +0530

    Added features section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import About from "../components/About"
+import Features from "../components/Features"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import NavBar from "../components/Navbar"
@@ -9,6 +10,7 @@ function HomePage() {
       <NavBar />
       <Hero />
       <About/>
+      <Features/>
       <Footer />
     </main>
   )

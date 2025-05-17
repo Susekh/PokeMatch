a1a155a63d05633b0ee12f2ea618b42d20668c8a
Subh <subhranshukhilar@outlook.com>
2025-05-18 01:34:07 +0530

    updated app route accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import GameContainer from "./components/play/GameContainer";
+import PlayGame from "./pages/PlayGame";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route
           path="/play"
-          element={<GameContainer/>}
+          element={<PlayGame/>}
         />
       </Routes>
       <Footer/>

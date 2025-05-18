a375c6d9c5f06ae45bb42cc590a1bba7f5cd829b
Subh <subhranshukhilar@outlook.com>
2025-05-19 03:40:04 +0530

    vite config to remove comments on build versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      format: {
+        comments: false, // remove all comments
+      },
+    },
+  },
 })

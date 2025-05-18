ddfa5f35948f26134c09df1d2285204e95d653b1
Subh <subhranshukhilar@outlook.com>
2025-05-19 03:38:56 +0530

    removed unused useEffect in gameBoard

--- src/components/play/GameContainer.tsx
+++ src/components/play/GameContainer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Trophy, ArrowLeft} from "lucide-react";
 import ScoreTracker from "./ScoreTracker";
 import LevelUpAnimation from "./LevelUpAnimation";

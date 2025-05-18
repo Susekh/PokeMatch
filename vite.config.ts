5cab5efc42c950b3483e4eed3c5108c4017eb321
Subh <subhranshukhilar@outlook.com>
2025-05-19 03:45:09 +0530

    vite config update to remove all console.* calls

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
       format: {
         comments: false, // remove all comments
       },
+      compress: {
+        drop_console: true, // remove all console.* calls
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.error', 'console.warn', 'console.info'], 
+      },
     },
   },
 })
